2da3e8b97289cde86fb24baa21aa4c490e6aaf01
Samuel Colburn <ho0ber@gmail.com>
2014-02-20 18:03:02 -0500

    fixing run.py bug

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from scraper.scraper import Scraper
 import json
 
+results = Scraper().scrape()
 res = json.loads(results[0])
 print json.dumps(res, sort_keys=False, indent=4, separators=(',', ': '))
